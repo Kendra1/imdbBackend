bd8c6bf1572453807b547e460b379311393b5a84
Kendra1 <ksydfl@gmail.com>
2019-08-13 16:54:11 +0200

    Made seeder for movies and genres

--- app/Genre.php
+++ app/Genre.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     public $timestamps = false;
+
+    const ITEMS = [
+        'epic fantasy', 'science fiction', 'comedy', 'action', 'horror', 'romance', 'mistery', 'drama',
+        'crime'
+    ];
+
+    protected $fillable = [
+        'name'
+    ];
+
+
+
+
 }
--- database/seeds/MovieTableSeeder.php
+++ database/seeds/MovieTableSeeder.php
@@ -3,11 +3,19 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Movie;
+use App\Genre;
 
 class MovieTableSeeder extends Seeder
 {
     public function run()
     {
-        factory(Movie::class, 10)->create();
+        foreach(Genre::ITEMS as $genreName) {
+            $genre = Genre::firstOrCreate([
+                'name' => $genreName
+            ]);
+            
+            factory(Movie::class, 5)->create(['genre_id' => $genre->id]);
+        
+        }
     }
 }
